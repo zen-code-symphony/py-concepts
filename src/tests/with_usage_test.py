be03a171e4e4048ab48a1e8b212c00f439f11e4d
zen.code.symphony <zen.code.symphony@gmail.com>
2024-01-02 11:42:40 +0530

    test: added fixture for mocked file calls.

--- src/tests/with_usage_test.py
+++ src/tests/with_usage_test.py
@@ -1,15 +1,23 @@
 from unittest.mock import Mock
 
+import pytest
+
 from app.with_usage import ManagedFile, managed_file
 
 _FILE_NAME = "filename.txt"
 
 
-def test_open_with_class_based_context_manager(mocker):
+@pytest.fixture
+def mocked_file_calls(mocker):
     mock_file = Mock()
     mock_open = mocker.mock_open(mock_file)
     mock_open.return_value = mock_file
     mocker.patch("builtins.open", mock_open)
+    return (mock_file, mock_open)
+
+
+def test_open_with_class_based_context_manager(mocked_file_calls):
+    mock_file, mock_open = mocked_file_calls
     file = ManagedFile(_FILE_NAME)
     with file as _:
         mock_open.assert_called_with(_FILE_NAME)
@@ -17,11 +25,8 @@ def test_open_with_class_based_context_manager(mocker):
     assert file.file is None
 
 
-def test_open_with_decorator_based_context_manager(mocker):
-    mock_file = Mock()
-    mock_open = mocker.mock_open(mock_file)
-    mock_open.return_value = mock_file
-    mocker.patch("builtins.open", mock_open)
+def test_open_with_decorator_based_context_manager(mocked_file_calls):
+    mock_file, mock_open = mocked_file_calls
     file = managed_file(_FILE_NAME)
     with file as _:
         mock_open.assert_called_with(_FILE_NAME)
