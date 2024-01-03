4904a58696c9c641886754683865f869f491a009
zen.code.symphony <zen.code.symphony@gmail.com>
2024-01-03 17:11:53 +0530

    chore: added main block to run the module directly.

--- src/app/string_format.py
+++ src/app/string_format.py
@@ -27,3 +27,12 @@ def format_using_formatted_string_literal(name):
 
 def format_using_template_strings(name):
     return Template("hello, $name").substitute(name=name)
+
+
+if __name__ == "__main__":
+    print(f"1. old style: {format_using_old_style_formatter('world')}")
+    print(f"2. new style format: {format_using_new_style_formatter('world')}")
+    print(
+        f"3. literal string interpolation: {format_using_formatted_string_literal('world')}"  # noqa
+    )
+    print(f"4. template strings: {format_using_template_strings('world')}")  # noqa
