64c386f4ec729655785e49bd875fe6d25459ecee
zen.code.symphony <zen.code.symphony@gmail.com>
2024-01-03 17:19:35 +0530

    chore: added main block to allow running the module directly.

--- src/app/under_dunder.py
+++ src/app/under_dunder.py
@@ -27,3 +27,22 @@ class UnderDunder:
 
     def return_temporary_increment_by_one(self, _):
         return _ + 1
+
+
+if __name__ == "__main__":
+    # Internal-use only but still accessible.
+    under_dunder = UnderDunder()
+    print(under_dunder._single_lead_under)
+
+    # Name mangling hides the real member name.
+    try:
+        print(under_dunder.__dunder)
+    except AttributeError:
+        print("Not accessible as name mangled")
+        print(under_dunder._UnderDunder__dunder)  # type: ignore
+
+    # Special methods allowed, but avoid using them.
+    print(under_dunder.__dunder__())
+
+    # Temporary name.
+    print(under_dunder.return_temporary_increment_by_one(1))
