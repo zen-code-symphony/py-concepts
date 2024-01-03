b7dfeb23fdfb9e48bbc5b37fddc0b56daf9f7d83
zen.code.symphony <zen.code.symphony@gmail.com>
2024-01-03 10:46:45 +0530

    test: added test for nested context manager use case.

--- src/tests/with_usage_test.py
+++ src/tests/with_usage_test.py
@@ -1,8 +1,8 @@
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import pytest
 
-from app.with_usage import ManagedFile, managed_file
+from app.with_usage import ManagedFile, PrintIndenter, managed_file
 
 _FILE_NAME = "filename.txt"
 
@@ -31,3 +31,22 @@ def test_open_with_decorator_based_context_manager(mocked_file_calls):
     with file as _:
         mock_open.assert_called_with(_FILE_NAME)
     mock_file.close.assert_called_once()
+
+
+@patch("builtins.print")
+def test_nested_context_management(mock_print):
+    with PrintIndenter() as indenter:
+        indenter.print("This")
+        mock_print.assert_called_with("    This")
+        assert indenter.level == 1
+        with indenter:
+            indenter.print("is")
+            mock_print.assert_called_with("        is")
+            assert indenter.level == 2
+            with indenter:
+                indenter.print("sooo")
+                mock_print.assert_called_with("            sooo")
+                assert indenter.level == 3
+        indenter.print("cool!")
+        mock_print.assert_called_with("    cool!")
+        assert indenter.level == 1
