3ad8eff3699179f84c6e113d35c2f6c064e18a03
Arun <arunkumarpg@hamaraindustries.xyz>
2024-01-09 13:47:54 +0530

    feat: added examples for function passed to / returned from a function.

--- src/app/function_object.py
+++ src/app/function_object.py
@@ -8,9 +8,46 @@ Key takeaways:
 
 
 def greet(text):
+    """Returns given test in upper case."""
     return f"{text}!".upper()
 
 
 def stored_functions():
     # Store functions in a data structure.
     return [greet, str.lower]
+
+
+def greet_using(func):
+    """Returns greeting by applying the passed function."""
+    return func("Hi there!")
+
+
+def convert_string_based_on_ask(text, is_lowercase):
+    """Converts the given string to upper or lower case using inner func."""
+
+    def upper(text: str) -> str:
+        return text.upper()
+
+    def lower(text: str) -> str:
+        return text.lower()
+
+    if is_lowercase:
+        return lower(text)
+    else:
+        return upper(text)
+
+
+def get_string_case_converter(converter_type):
+    """Returns a string converter for the requested type."""
+
+    def upper(text: str) -> str:
+        return text.upper()
+
+    def lower(text: str) -> str:
+        return text.lower()
+
+    if converter_type == "lower":
+        return lower
+    elif converter_type == "upper":
+        return upper
+    return None
--- src/tests/function_object_test.py
+++ src/tests/function_object_test.py
@@ -1,6 +1,12 @@
 import pytest
 
-from app.function_object import greet, stored_functions
+from app.function_object import (
+    convert_string_based_on_ask,
+    get_string_case_converter,
+    greet,
+    greet_using,
+    stored_functions,
+)
 
 
 def test_function_name_and_object_are_separate_concerns():
@@ -32,3 +38,26 @@ def test_functions_can_be_stored_in_data_structures():
     # They can be called without assigning to variable.
     assert funcs[0]("hi") == "HI!"  # greet
     assert funcs[1]("HI") == "hi"  # str.lower
+
+
+def test_function_can_be_passed_to_other_function():
+    def loud(text: str) -> str:
+        return text.upper()
+
+    def soft(text: str) -> str:
+        return text.lower()
+
+    assert greet_using(soft) == "hi there!"
+
+
+def test_function_can_be_nested():
+    text = "Hello World"
+    assert convert_string_based_on_ask(text, is_lowercase=True) == text.lower()
+    assert convert_string_based_on_ask(text, is_lowercase=False) == text.upper()
+
+
+def test_function_can_be_returned():
+    lower_func = get_string_case_converter("lower")
+    assert lower_func("HELLO") == "hello"
+    upper_func = get_string_case_converter("upper")
+    assert upper_func("hello") == "HELLO"
